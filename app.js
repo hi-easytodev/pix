64133ce217a91ad06094a8c9455bacc12f4866bd
hi-easytodev <easytodev.go@gmail.com>
2025-05-01 23:00:33 -0500

    Добавила переменные о себе в app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const firstName ='Larisa'
+const age = 5;
+const city = 'Москва';
+const aboutMe = 'Жру успокоительные после вашего урока';
 console.log('Hello', firstName)
+console.log('Возраст:', age);
+console.log('Город:', city);
+console.log('Обо мне:', aboutMe);

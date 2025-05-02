f55e43fb1007fe4da07947cf84c2735fc4816b87
hi-easytodev <easytodev.go@gmail.com>
2025-05-02 18:19:06 -0500

    Добавила домашку по уроку 3: переменные и папку lesson_3

---
+++ lesson_3.js
@@ -0,0 +1,13 @@
+const titleProject = 'Оценка стоимости';
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+const screenPrice = 5000;
+const percentage = 20;
+const fullPrice = 100000;
+const responsive = true;
+
+console.log('Название проекта:', titleProject);
+console.log('Типы экранов:', screensValue);
+console.log('Цена одного экрана:', screenPrice);
+console.log('Процент отката:', percentage);
+console.log('Желаемая сумма:', fullPrice);
+console.log('Адаптивность:', responsive);
